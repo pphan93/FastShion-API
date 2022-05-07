8bdaae3aaecfc16401635d2b5b8528bd64036a31
Phuoc Phan <phuocphan12@gmail.com>
2022-05-07 14:08:39 -0400

    update code for stripe

--- routes/stripe.js
+++ routes/stripe.js
@@ -4,22 +4,44 @@ const dotenv = require("dotenv");
 dotenv.config();
 const stripe = require("stripe")(process.env.STRIPE_KEY);
 
+const calculateOrderAmount = (items) => {
+  // Replace this constant with a calculation of the order's amount
+  // Calculate the order total on the server to prevent
+  // people from directly manipulating the amount on the client
+  return 1400;
+};
+
 //payment using stripe
 router.post("/payment", (req, res) => {
-  stripe.charges.create(
-    {
-      source: req.body.tokenId.id,
-      amount: req.body.amount,
-      currency: "cad",
+  const { items } = req.body;
+
+  // Create a PaymentIntent with the order amount and currency
+  const paymentIntent = await stripe.paymentIntents.create({
+    amount: calculateOrderAmount(items),
+    currency: "cad",
+    automatic_payment_methods: {
+      enabled: true,
     },
-    (stripeError, stripeRes) => {
-      if (stripeError) {
-        res.status(500).json(stripeError);
-      } else {
-        res.status(200).json(stripeRes);
-      }
-    }
-  );
+  });
+
+  res.send({
+    clientSecret: paymentIntent.client_secret,
+  });
+
+  // stripe.charges.create(
+  //   {
+  //     source: req.body.tokenId.id,
+  //     amount: req.body.amount,
+  //     currency: "cad",
+  //   },
+  //   (stripeError, stripeRes) => {
+  //     if (stripeError) {
+  //       res.status(500).json(stripeError);
+  //     } else {
+  //       res.status(200).json(stripeRes);
+  //     }
+  //   }
+  // );
 });
 
 module.exports = router;
